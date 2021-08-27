a77b1b2becd049e732fb1d3e8a5fe87d1af102d7
abel <aamarquele@gmail.com>
2021-08-27 20:09:12 +0200

    Permitir que a app do modelo questionario seja visivel no painel do admin

--- questionario/admin.py
+++ questionario/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Pergunta, Quintil_Riqueza, historico_Saude
+
 # Register your models here.
+
+admin.site.register(Pergunta)
+admin.site.register(Quintil_Riqueza)
+admin.site.register( historico_Saude)
+
