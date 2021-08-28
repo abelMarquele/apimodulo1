22730854b87e162285ccd401da4953b71b2496ea
abel <aamarquele@gmail.com>
2021-08-28 16:18:26 +0200

    Modificar o modelo accounts da app acounts

--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,25 @@
 from django.db import models
-from authemail.models import EmailUserManager, EmailAbstractUser
+from django.utils.translation import ugettext_lazy as _
+
+from authemail.models import EmailAbstractUser, EmailUserManager
+
 
 class MyUser(EmailAbstractUser):
-	# Custom fields
-	date_of_birth = models.DateField('Date of birth', null=True, blank=True)
+    # Custom fields
+    date_of_birth = models.DateField(_('Date of birth'), null=True, blank=True)
 
-	# Required
-	objects = EmailUserManager()
+    # Required
+    objects = EmailUserManager()
+
+
+class VerifiedUserManager(EmailUserManager):
+    def get_queryset(self):
+        return super(VerifiedUserManager, self).get_queryset().filter(
+            is_verified=True)
+
+
+class VerifiedUser(MyUser):
+    objects = VerifiedUserManager()
+
+    class Meta:
+        proxy = True
